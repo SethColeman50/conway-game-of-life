380ff30b53520ad413df8984b4878cbfae864d9e
Seth Coleman <84861800+SethColeman50@users.noreply.github.com>
2024-04-29 13:57:18 -0400

    changed absolute paths to use $HOME

--- data_gathering/serial_sbatch.sh
+++ data_gathering/serial_sbatch.sh
@@ -3,6 +3,5 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=8G
 #SBATCH -t 02:00:00
-#SBATCH -D /home/bingamanz/conway-game-of-life
 
-/home/bingamanz/conway-game-of-life/serial /home/bingamanz/conway-game-of-life/examples/$filename /dev/null 1000
+$HOME/conway-game-of-life/serial $HOME/conway-game-of-life/examples/$filename /dev/null 1000
